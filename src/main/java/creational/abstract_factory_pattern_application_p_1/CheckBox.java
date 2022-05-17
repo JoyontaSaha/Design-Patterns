bcf9e69aa38bcaeba98257f6fff662900a7419b8
JoyontaSaha <Joyontasaha8@gmail.com>
2022-05-17 21:58:28 +0600

    Delete src/main/java/creational/abstract_factory_pattern_application_p_1 directory

--- src/main/java/creational/abstract_factory_pattern_application_p_1/CheckBox.java
+++
@@ -1,5 +0,0 @@
-package creational.abstract_factory_pattern_application_p_1;
-
-public interface CheckBox {
-    void select();
-}
--- src/main/java/creational/abstract_factory_pattern_application_p_1/OSName.java
+++
@@ -1,6 +0,0 @@
-package creational.abstract_factory_pattern_application_p_1;
-
-public enum OSName {
-    WINDOWS,
-    LINUX
-}
